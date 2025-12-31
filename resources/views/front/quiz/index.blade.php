bc6f3fa26e39e6b8d4b129e25ce6a34bb8e0b78f
vietvt <daothienthanhduong1@gmail.com>
2025-12-31 08:50:07 +0700

    quiz1

--- resources/views/front/quiz/index.blade.php
+++ resources/views/front/quiz/index.blade.php
@@ -6,6 +6,7 @@
             min-height: 100vh;
             background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
             padding: 40px 0;
+            margin-top: 100px;
         }
 
         .quiz-header {
