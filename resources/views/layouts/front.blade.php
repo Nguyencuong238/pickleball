f654f6826afa690a86ee35249134d82851211b73
Pham Thao <phamthao03596@gmail.com>
2026-03-27 15:10:41 +0700

    feat: add Zalo platform site verification meta tag

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+    <meta name="zalo-platform-site-verification" content="HVkzTfNl77DAsxbOgQKqPMc4cd65yaylEJ4n" />
     @hasSection('seo')
         @yield('seo')
     @else
